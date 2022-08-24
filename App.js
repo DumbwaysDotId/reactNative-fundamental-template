b09b69ecf75e0bcaf714ee4d08c9416ae083984a
Your Name <samsulr99@gmail.com>
2022-08-24 10:04:52 +0700

    update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
+import State from "./src/screens/state";
 
 //Import Screen
 
@@ -9,6 +10,7 @@ export default function App() {
     <View>
       <StatusBar />
       {/* Use Component */}
+      <State />
     </View>
   );
 }
