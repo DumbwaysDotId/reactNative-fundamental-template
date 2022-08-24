771ecc46f054b6b431aae443eb051eeeffdff41b
Your Name <samsulr99@gmail.com>
2022-08-24 13:01:27 +0700

    update

--- Container.js
+++ Container.js
@@ -2,7 +2,10 @@ import * as React from "react";
 import { Text, Box } from "native-base";
 
 // Import Screen
+import Hello from './src/screens/hello'
+import Form from './src/screens/formNativeBase'
+
 
 export default function Container() {
-  return; //   Use Screen
+  return <Form />
 }
--- src/screens/formNativeBase.js
+++ src/screens/formNativeBase.js
@@ -18,5 +18,39 @@ import {
 } from "native-base";
 
 export default function FormNativeBase() {
-  return <Box>{/* Code Here */}</Box>;
+  return(
+
+    <Box>
+      <Heading color="primary.900">
+        Welcome
+      </Heading>  
+
+      <Heading color="primary.600" size="lg">
+        Sign in to continue!
+      </Heading>
+
+      <VStack space={2} mt={5}>
+        <FormControl.Label _text={{color: 'primary.800', fontWeight: 'bold', fontSize: 'sm'}}>
+          Email
+        </FormControl.Label>
+        <Input />
+        <FormControl.Label _text={{color: 'primary.800', fontWeight: 'bold', fontSize: 'sm'}}>
+          Password
+        </FormControl.Label>
+        <Input type="password" />
+
+        <Link mt={2} _text={{fontSize: 'xs', fontWeight:'700', color:'primary.600'}}>
+          Forgot Password?
+        </Link>
+
+      </VStack>
+
+      <VStack colorSchema="primary" mt={5} _text={{color: 'white'}}>
+        <Button>Login</Button>
+      </VStack>
+
+
+    </Box>
+
+  )
 }
