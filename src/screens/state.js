6ac48955aea25f56120cd0cbeb03d7dd5839c2a0
Your Name <samsulr99@gmail.com>
2022-08-30 14:38:35 +0700

    Revert "update"
    This reverts commit b09b69ecf75e0bcaf714ee4d08c9416ae083984a.

--- src/screens/state.js
+++ src/screens/state.js
@@ -1,30 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function State() {
   // Init State
-  const [counter, setCounter] = useState(0)
 
   //Create Function Add
-  function Add(){
-    return setCounter(counter + 1)
-  }
 
   //Create Function Less
-  function Less(){
-    if(counter > 0){
-      return setCounter(counter - 1)
-    }
-  }
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
-      <Text>{counter}</Text>
-      <TouchableOpacity onPress={Add}>Add</TouchableOpacity>
-      <TouchableOpacity onPress={Less}>Less</TouchableOpacity>
     </View>
   );
 }
